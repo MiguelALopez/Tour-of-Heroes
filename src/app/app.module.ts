3662ce96360fdf69ba50e39e5e439675491c760a
Miguel Angel Lopez <miguel.angel.lopez@correounivalle.edu.co>
2017-06-14 23:56:25 +0000

    Created the routing module and separated from app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule}   from '@angular/forms'; // <-- NgModel lives here
-import {RouterModule}   from '@angular/router';
 
 import {AppComponent}  from './app.component';
 import {HeroDetailComponent} from './Components/hero-detail/hero-detail.component';
@@ -10,30 +9,13 @@ import {DashboardComponent} from './Components/dashboard/dashboard.component';
 
 import {HeroService} from './Services/hero.service';
 
+import {AppRoutingModule} from './Modules/app-routing.module';
+
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-        RouterModule.forRoot([
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-                
-            },
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'detail/:id',
-                component: HeroDetailComponent
-            }
-        ])
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
