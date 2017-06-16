58c8f4701f6c9ed9e0bb546816cc8c7f337b92ea
Miguel Angel Lopez <miguel.angel.lopez@correounivalle.edu.co>
2017-06-16 09:23:15 -0500

    Added de methods create, update and delete for the database service; Implemented the functions for the UI to create, delete and update a hero; Added some styles and reformat the code

--- src/app/Services/in-memory-data.service.ts
+++ src/app/Services/in-memory-data.service.ts
@@ -1,14 +1,14 @@
-import { InMemoryDbService } from 'angular-in-memory-web-api';
+import {InMemoryDbService} from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
-  createDb() {
-    const heroes = [
-      {id: 11, name: 'Hozo'},
-      {id: 12, name: 'Tafur'},
-      {id: 13, name: 'Boku'},
-      {id: 14, name: 'Pico'},
-      {id: 15, name: 'Walda'},
-      {id: 16, name: '28'}
-    ];
-    return {heroes};
-  }
+    createDb() {
+        const heroes = [
+            {id: 1, name: 'Hozo'},
+            {id: 2, name: 'Tafur'},
+            {id: 3, name: 'Boku'},
+            {id: 4, name: 'Pico'},
+            {id: 5, name: 'Walda'},
+            {id: 6, name: '28'}
+        ];
+        return {heroes};
+    }
 }
